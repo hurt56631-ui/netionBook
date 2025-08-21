4582eeb8ae21a284b028c24efd22c9ef53dc2065
Hurtg185 <91710543+Hurtg185@users.noreply.github.com>
2025-08-22 02:57:53 +0630

    Update PostCopyright.js

--- themes/heo/components/PostCopyright.js
+++ themes/heo/components/PostCopyright.js
@@ -1,5 +1,7 @@
-// themes/heo/components/PostCopyright.js (文字居中版)
+// themes/heo/components/PostCopyright.js (图片版)
 import { siteConfig } from '@/lib/config'
+import LazyImage from '@/components/LazyImage'
+import SmartLink from '@/components/SmartLink'
 import CONFIG from '../config'
 
 export default function PostCopyright() {
@@ -7,12 +9,10 @@ export default function PostCopyright() {
     return <></>
   }
   return (
-    <section className='dark:text-gray-300 mt-6 mx-1 '>
-      <ul className='text-center overflow-x-auto whitespace-nowrap text-sm dark:bg-gray-900 bg-gray-100 p-5 leading-8 border-l-2 border-indigo-500 rounded-xl'>
-        <li><strong>课程咨询:</strong> 请通过顶部社交媒体图标联系我们。</li>
-        <li><strong>版权声明:</strong> 本站所有教学内容均为原创，未经许可，禁止转载。</li>
-        <li><strong>特别说明:</strong> 学习效果因人而异，请根据自身情况选择合适的课程。</li>
-      </ul>
+    <section className='mt-6 mx-1'>
+      <SmartLink href="/contact-page"> {/* (可选) 为图片添加链接 */}
+        <LazyImage src="/images/copyright-info.png" alt="课程咨询与版权信息" className="w-full h-auto rounded-xl" />
+      </SmartLink>
     </section>
   )
 }
